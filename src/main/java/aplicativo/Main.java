0aabaa259a1c4ee25423075e2cf1e8c1a34a926e
teusiaqe <jmokatavares@gmail.com>
2025-07-30 19:43:45 -0300

    configuração basica

---
+++ src/main/java/aplicativo/Main.java
@@ -0,0 +1,14 @@
+package aplicativo;
+
+import dominio.Pessoa;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Pessoa p1  = new Pessoa(1, "carlos", "carlos@gmail.com");
+		
+		System.out.println(p1);
+
+	}
+
+}
